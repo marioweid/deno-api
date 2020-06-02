003a284062ed4fdcc93ade44e11c8d313fb5db89
marioweidnerpartner <Mario.Weidner@partner.bmw.de>
2020-06-02 16:53:55 +0200

    added mongodb container

--- db/collections.ts
+++ db/collections.ts
@@ -5,8 +5,11 @@ import {
   config,
 } from "../deps.ts";
 
+console.log(config().MONGO_URL);
+
 const client: MongoClient = new MongoClient();
 client.connectWithUri(config().MONGO_URL);
-const database: Database = client.database("deno-db");
+console.log(await client.listDatabases());
 
+const database: Database = client.database("deno-db");
 export const users: Collection = database.collection("users");
